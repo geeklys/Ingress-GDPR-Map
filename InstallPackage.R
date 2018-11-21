5a36bcf51fd03ce08bb4cd34c85a718311b354a5
geeklys <geeklys@gmail.com>
2018-11-21 21:55:48 +0800

    Add files via upload

---
+++ InstallPackage.R
@@ -0,0 +1,4 @@
+
+install.packages('data.table',repos = 'https://mirrors.tongji.edu.cn/CRAN/')
+install.packages('devtools',repos = 'https://mirrors.tongji.edu.cn/CRAN/')
+devtools::install_github('lchiffon/REmap')
